90b9d3f8e0c9a36775156a1f6022e3bdbde243c4
ali <alidevops1504@gmail.com>
2025-08-11 22:31:09 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        viler: "#43c567",
+      },
+    },
+  },
+  plugins: [],
+};
