1516e9a64442339c30300c5d8b4dc88634235c0c
Your Name <your_email@example.com>
2025-09-30 09:49:54 +0330

    change some applied

--- app/(main)/map/index.jsx
+++ app/(main)/map/index.jsx
@@ -1,34 +1,42 @@
 import {  Text, View } from 'react-native'
 import React from 'react'
-import MapView, { Marker, UrlTile ,PROVIDER_DEFAULT} from 'react-native-maps'
+import { Ionicons } from '@expo/vector-icons';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import { WebView } from 'react-native-webview';
-export default function map() {
+import searchBar from '../../../components/SearchBar'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import messages from '../../../components/messages'
+import support from '../../../components/support'
+const Tab=createBottomTabNavigator()
+const Stack = createNativeStackNavigator();
+ function map() {
   return (
-    <View className='flex flex-1 mt-10' >
-{/* 
-<MapView
-  style={{ flex: 1 }}
-   provider={PROVIDER_DEFAULT}
-  initialRegion={{
-    latitude: 35.6892,
-    longitude: 51.3890,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  }}
->
-  <UrlTile
-    urlTemplate="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-    maximumZ={19}
-  />
-    <Marker
-          coordinate={{ latitude: 35.6892, longitude: 51.3890 }}
-          title="تهران"
-        />
-</MapView> */}
-    <WebView source={{ uri: 'https://www.openstreetmap.org/#map=6/32.72/53.68' }}
+    <>
+    <View className='flex flex-1' >
+    <WebView  originWhitelist={['*']} source={require('../../map.html')}
         style={{ flex: 1 }} />
-    <Text className='mt-10'>salam</Text>
+
     </View>
+    </>
+
+  )
+}
+
+export default function nav(){
+  return(
+      <Tab.Navigator screenOptions={({route})=>({tabBarIcon:({color,size})=>{
+       <Ionicons name='home' size={size} color={color} />
+      }})}>
+        <Tab.Screen name='پشتیبانی' options={{headerShown:false}} component={searchBar} ></Tab.Screen>
+        <Tab.Screen  name='طرح تشویقی' options={{headerShown:false}} component={support} ></Tab.Screen>
+        <Tab.Screen  name='پیام ها' options={{headerShown:false}} component={messages} ></Tab.Screen>
+        <Tab.Screen options={{
+          tabBarIcon:({color,size})=>(
+            <Ionicons name='home' size={size} color={color} />
+          ),headerShown:false
+        }}  name='خانه'  component={map} ></Tab.Screen>
+      </Tab.Navigator>
   )
 }
 
