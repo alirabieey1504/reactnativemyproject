430f7a3be0a47705530a908efd985bb4a8fc1f8a
Your Name <your_email@example.com>
2025-09-30 12:00:00 +0330

    change some applied from this

--- app/(main)/map/index.jsx
+++ app/(main)/map/index.jsx
@@ -1,11 +1,9 @@
 import {  Text, View } from 'react-native'
-import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import { WebView } from 'react-native-webview';
 import searchBar from '../../../components/SearchBar'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 import messages from '../../../components/messages'
 import support from '../../../components/support'
 const Tab=createBottomTabNavigator()
@@ -14,23 +12,35 @@ const Stack = createNativeStackNavigator();
   return (
     <>
     <View className='flex flex-1' >
-    <WebView  originWhitelist={['*']} source={require('../../map.html')}
-        style={{ flex: 1 }} />
-
+    <WebView onP  originWhitelist={['*']} source={require('../../map.html')}
+        style={{ flex: 1 }} onMessage={(event) => {
+    const data = JSON.parse(event.nativeEvent.data);
+    console.log('مختصات انتخاب شده:', data);
+  }} />
+      
     </View>
     </>
 
   )
 }
+function turnOn(){
+  return(
+   <Text>hello</Text>
+  )
+}
 
 export default function nav(){
   return(
-      <Tab.Navigator screenOptions={({route})=>({tabBarIcon:({color,size})=>{
-       <Ionicons name='home' size={size} color={color} />
-      }})}>
+      <Tab.Navigator initialRouteName='خانه'>
         <Tab.Screen name='پشتیبانی' options={{headerShown:false}} component={searchBar} ></Tab.Screen>
         <Tab.Screen  name='طرح تشویقی' options={{headerShown:false}} component={support} ></Tab.Screen>
-        <Tab.Screen  name='پیام ها' options={{headerShown:false}} component={messages} ></Tab.Screen>
+       
+        <Tab.Screen 
+         name='turnon' options={{headerShown:false ,tabBarLabel:()=>null,tabBarIcon:({color,size})=>(
+            <Ionicons className='mt-1' name='power' size={size} color={color} />
+         )}}component={map} ></Tab.Screen>
+
+        <Tab.Screen name='پیام ها' options={{headerShown:false}} component={messages} ></Tab.Screen>
         <Tab.Screen options={{
           tabBarIcon:({color,size})=>(
             <Ionicons name='home' size={size} color={color} />
