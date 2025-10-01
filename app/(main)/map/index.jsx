5915b564a4151a03e3014e182a5d435dc136838e
Your Name <your_email@example.com>
2025-10-01 14:33:54 +0330

    change some applied from map

--- app/(main)/map/index.jsx
+++ app/(main)/map/index.jsx
@@ -1,4 +1,4 @@
-import {  Text, View } from 'react-native'
+import {  BackHandler, Text, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import { WebView } from 'react-native-webview';
@@ -6,17 +6,41 @@ import searchBar from '../../../components/SearchBar'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import messages from '../../../components/messages'
 import support from '../../../components/support'
+import React, { useRef } from 'react';
 const Tab=createBottomTabNavigator()
 const Stack = createNativeStackNavigator();
- function map() {
+ function Map() {
+  const webviewRef=useRef(null)
+
+  React.useEffect(()=>{
+   
+   const backAction=()=>{
+
+    if(webviewRef.current){
+      webviewRef.current.injectJavaScript(`window.resetSelection();true;`)
+      console.log('work');
+      return true
+    }
+    return false
+   }
+
+
+   const backHandle= BackHandler.addEventListener('hardwareBackPress',backAction)
+    return ()=> backHandle.remove()
+  },[])
+
+  const handleMessage=(event)=>{
+    console.log('this is test');
+    console.log(JSON.parse(event.nativeEvent.data),'this is data');
+  }
   return (
     <>
     <View className='flex flex-1' >
-    <WebView onP  originWhitelist={['*']} source={require('../../map.html')}
-        style={{ flex: 1 }} onMessage={(event) => {
-    const data = JSON.parse(event.nativeEvent.data);
-    console.log('مختصات انتخاب شده:', data);
-  }} />
+    <WebView  
+    ref={webviewRef}
+       originWhitelist={['*']} source={require('../../map.html')}
+        style={{ flex: 1 }} onMessage={handleMessage}
+        onLoad={console.log('this load for')} />
       
     </View>
     </>
@@ -38,14 +62,14 @@ export default function nav(){
         <Tab.Screen 
          name='turnon' options={{headerShown:false ,tabBarLabel:()=>null,tabBarIcon:({color,size})=>(
             <Ionicons className='mt-1' name='power' size={size} color={color} />
-         )}}component={map} ></Tab.Screen>
+         )}}component={Map} ></Tab.Screen>
 
         <Tab.Screen name='پیام ها' options={{headerShown:false}} component={messages} ></Tab.Screen>
         <Tab.Screen options={{
           tabBarIcon:({color,size})=>(
             <Ionicons name='home' size={size} color={color} />
           ),headerShown:false
-        }}  name='خانه'  component={map} ></Tab.Screen>
+        }}  name='خانه'  component={Map} ></Tab.Screen>
       </Tab.Navigator>
   )
 }
